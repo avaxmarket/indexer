d61d99c732522b85b002e12be5bdf362a6909fa3
avax <avaxmarket@gmail.com>
2023-12-01 22:21:57 +0800

    fix: confirmed

--- src/db/select/utxo.ts
+++ src/db/select/utxo.ts
@@ -4,7 +4,7 @@ import { pool } from "../pool"
 
 export const get_avax_insc_utxo_info = (txid: string, index: number): Promise<db_utxo_info> => {
     return new Promise((resolve, reject) => {
-        pool.query(`SELECT * FROM utxo WHERE txid = ? AND  \`index\` = ?`, [txid, index], (err, result) => {
+        pool.query(`SELECT * FROM utxo WHERE txid = ? AND  \`index\` = ? AND confirmed = ?`, [txid, index, 1], (err, result) => {
             if(err){
                 reject(err)
                 return
