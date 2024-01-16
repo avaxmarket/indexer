4cce0ad029d24f109e7bd945be6df23570169901
avaxmarket <151865899+avaxmarket@users.noreply.github.com>
2024-01-16 21:45:28 +0800

    Update next_transfer_block.ts
    Fix the problem of multiple identical UTXO inputs in the same transfer

--- src/next_transfer_block.ts
+++ src/next_transfer_block.ts
@@ -65,7 +65,12 @@ const main = async (
                                 let isTransfer = true
                                 let input_value = new BigNumber(0)
                                 let output_value = new BigNumber(0)
+                                let spent_utxo_flag_list: string[] = []
                                 for await (const input of inscription.vin) {
+                                    if(spent_utxo_flag_list.includes(utxo_id)){
+                                        isTransfer = false
+                                        continue
+                                    }
                                     const utxo = await get_avax_insc_utxo_info(input.txid, parseInt(input.vout))
                                     if (utxo?.owner?.toLocaleLowerCase() !== from) {
                                         isTransfer = false
